28266bd736c7e36c2ddb9fcd3f2c79801c0331be
Sean Gray <seangray.dev@gmail.com>
2024-02-14 14:27:52 -0500

    Add SheetClose component to DocumentBtn

--- src/components/layout/Header/document-button.tsx
+++ src/components/layout/Header/document-button.tsx
@@ -1,19 +1,22 @@
 import { Button } from '@/components/ui/button';
+import { SheetClose } from '@/components/ui/sheet';
 import { Document } from '@/lib/constants';
 import { FileIcon } from 'lucide-react';
 
 export const DocumentBtn: React.FC<Document> = ({ name, date }) => {
   return (
-    <Button
-      variant={'ghost'}
-      className='flex gap-4 items-center justify-start w-full hover:bg-transparent group'>
-      <FileIcon className='text-white' size={22} />
-      <div className='flex flex-col gap-1 text-muted-foreground'>
-        <span className='text-left'>{date}</span>
-        <span className='text-white group-hover:text-primary duration-150 transition-all'>
-          {name}
-        </span>
-      </div>
-    </Button>
+    <SheetClose asChild>
+      <Button
+        variant={'ghost'}
+        className='flex gap-4 items-center justify-start w-full hover:bg-transparent group'>
+        <FileIcon className='text-white' size={22} />
+        <div className='flex flex-col gap-1 text-muted-foreground'>
+          <span className='text-left'>{date}</span>
+          <span className='text-white group-hover:text-primary duration-150 transition-all'>
+            {name}
+          </span>
+        </div>
+      </Button>
+    </SheetClose>
   );
 };
