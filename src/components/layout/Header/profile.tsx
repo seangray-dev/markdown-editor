ddd33f0bcfc93b1b37f0b315fe9523083b9fa6c7
Sean Gray <seangray.dev@gmail.com>
2024-02-15 16:08:46 -0500

    Add user profile and signout functionality to header

--- src/components/layout/Header/profile.tsx
+++ src/components/layout/Header/profile.tsx
@@ -1,3 +1,38 @@
+import SignoutBtn from '@/components/buttons/SignoutBtn';
+import { supabase } from '@/utils/supabase/client';
+import { User } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+
 export default function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data: response, error } = await supabase.auth.getUser();
+
+      if (error) {
+        console.error('Error fetching user:', error);
+        return;
+      }
+
+      if (response.user) {
+        setUser(response.user);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <div className='flex flex-col w-full gap-4 border-t border-t-muted pt-4'>
+      {user && (
+        <div>
+          <p className='text-muted-foreground'>
+            You are logged in as: {user.user_metadata?.full_name || user.email}!
+          </p>
+        </div>
+      )}
+      <SignoutBtn />
+    </div>
+  );
 }
