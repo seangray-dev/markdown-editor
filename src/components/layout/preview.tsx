04fad9bc58f3f8e43ecb256faedbe8e0d590e77c
Sean Gray <seangray.dev@gmail.com>
2024-02-14 20:10:40 -0500

    Add Roboto Slab font and update section styling

--- src/components/layout/preview.tsx
+++ src/components/layout/preview.tsx
@@ -1,8 +1,15 @@
 import Markdown from 'markdown-to-jsx';
+import { Roboto_Slab } from 'next/font/google';
+
+const robotoSlab = Roboto_Slab({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+});
 
 export default function Preview({ markdown }: any) {
   return (
-    <section className='px-5 py-4'>
+    <section
+      className={`custom-bullets text-sm px-5 py-4 prose max-w-none prose-stone prose-md dark:prose-invert prose-h1:text-[32px] prose-h2:font-light prose-h2:text-[28px] prose-h3:text-[24px] prose-h4:text-[20px] prose-h5:text-[16px] prose-p:leading-6 prose-h5:font-bold prose-h6:text-primary prose-h6:font-bold ${robotoSlab.className}`}>
       <Markdown>{markdown}</Markdown>
     </section>
   );
