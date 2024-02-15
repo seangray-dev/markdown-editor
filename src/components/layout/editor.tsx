d2a90394da5e39ea63cc8ffd161c7d2a2e55a4a8
Sean Gray <seangray.dev@gmail.com>
2024-02-14 20:10:35 -0500

    Add Roboto Mono font and update Editor styling

--- src/components/layout/editor.tsx
+++ src/components/layout/editor.tsx
@@ -1,6 +1,13 @@
 'use client';
 
+import { Roboto_Mono } from 'next/font/google';
 import { Textarea } from '../ui/textarea';
+
+const robotoMono = Roboto_Mono({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
 export default function Editor({ markdown, setMarkdown }: any) {
   const handleChange = (e: any) => {
     setMarkdown(e.target.value);
@@ -9,7 +16,7 @@ export default function Editor({ markdown, setMarkdown }: any) {
   return (
     <section className='h-full border-r'>
       <Textarea
-        className='w-full h-full px-5 py-4 text-base focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none border-none'
+        className={`${robotoMono.className} leading-6 resize-none h-full px-5 py-4 text-sm focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none border-none`}
         value={markdown}
         onChange={handleChange}
       />
