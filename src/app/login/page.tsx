45aab24ea7e596ddae028f6e6797a56edaa7dc02
Sean Gray <seangray.dev@gmail.com>
2024-02-15 01:06:12 -0500

    Add login and signup pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,99 @@
+import GitHubSignInBtn from '@/components/buttons/GitHubSignInBtn';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+export default function Login({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+  const signIn = async (formData: FormData) => {
+    'use server';
+
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return redirect('/login?message=Could not authenticate user');
+    }
+
+    return redirect('/');
+  };
+
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <Card className='flex-1 flex flex-col p-8 max-w-md justify-center'>
+        <CardHeader className='p-0 mb-4'>
+          <CardTitle>
+            <h1 className='text-3xl font-bold'>Login</h1>
+          </CardTitle>
+        </CardHeader>
+        <CardDescription className='mb-4'>
+          <p>Add your details below to get back into the app</p>
+        </CardDescription>
+        <CardContent className='p-0'>
+          <form
+            className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'
+            action={signIn}>
+            <Label className='text-md' htmlFor='email'>
+              Email
+            </Label>
+            <Input
+              className='rounded-md px-4 py-2 bg-inherit border mb-6'
+              name='email'
+              placeholder='you@example.com'
+              required
+            />
+            <Label className='text-md' htmlFor='password'>
+              Password
+            </Label>
+            <Input
+              className='rounded-md px-4 py-2 bg-inherit border mb-6'
+              type='password'
+              name='password'
+              placeholder='••••••••'
+              required
+            />
+            <Button className='rounded-md px-4 py-2 text-foreground mb-2'>
+              Sign In
+            </Button>
+            {searchParams?.message && (
+              <p className='mt-4 p-4 bg-foreground/10 text-foreground text-center'>
+                {searchParams.message}
+              </p>
+            )}
+          </form>
+          <GitHubSignInBtn />
+        </CardContent>
+        <CardFooter className='p-0'>
+          <div className='flex gap-1 mt-4 justify-center mx-auto'>
+            <p>Don't have an account?</p>
+            <Link
+              className='hover:underline hover:text-primary duration-75 transition-all'
+              href='/signup'>
+              Sign Up
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,129 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { createClient } from '@/utils/supabase/server';
+import { headers } from 'next/headers';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+export default function SignUp({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+  const isSuccess =
+    searchParams?.message === 'Check email to continue sign in process';
+  const signUp = async (formData: FormData) => {
+    'use server';
+
+    const origin = headers().get('origin');
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirm-password') as string;
+
+    if (password.length < 8) {
+      return redirect(
+        '/signup?message=Password must be at least 8 characters long'
+      );
+    }
+
+    if (password !== confirmPassword) {
+      return redirect('/signup?message=Passwords do not match');
+    }
+
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      return redirect('/signup?message=Could not authenticate user');
+    }
+
+    return redirect('/signup?message=Check email to continue sign in process');
+  };
+
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <Card className='flex-1 flex flex-col p-8 max-w-md justify-center'>
+        <CardHeader className='p-0 mb-4'>
+          <CardTitle>
+            <h1 className='text-3xl font-bold'>Create account</h1>
+          </CardTitle>
+        </CardHeader>
+        <CardDescription className='mb-4'>
+          <p>Start your journey with us</p>
+        </CardDescription>
+        <CardContent className='p-0'>
+          <form className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'>
+            <Label className='text-md' htmlFor='email'>
+              Email
+            </Label>
+            <Input
+              className='rounded-md px-4 py-2 bg-inherit border mb-6'
+              name='email'
+              placeholder='you@example.com'
+              required
+            />
+            <Label className='text-md' htmlFor='password'>
+              Create Password
+            </Label>
+            <Input
+              className='rounded-md px-4 py-2 bg-inherit border mb-6'
+              type='password'
+              name='password'
+              placeholder='••••••••'
+              required
+            />
+            <Label className='text-md' htmlFor='confirm-password'>
+              Confirm Password
+            </Label>
+            <Input
+              className='rounded-md px-4 py-2 bg-inherit border mb-6'
+              type='password'
+              name='confirm-password'
+              placeholder='••••••••'
+              required
+            />
+            <Button
+              formAction={signUp}
+              className='border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2'>
+              Sign Up
+            </Button>
+            {searchParams?.message && (
+              <p
+                className={`mt-4 p-1 text-center text-white ${
+                  isSuccess ? 'bg-secondary' : 'bg-destructive'
+                }`}>
+                {searchParams.message}
+              </p>
+            )}
+          </form>
+        </CardContent>
+        <CardFooter className='p-0'>
+          <div className='flex gap-1 mt-4 justify-center mx-auto'>
+            <p>Already have an account?</p>
+            <Link
+              className='hover:underline hover:text-primary duration-75 transition-all'
+              href='/login'>
+              Log In
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
